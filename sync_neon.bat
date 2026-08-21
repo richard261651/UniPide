df56f7c2ea6aac8256d164bc081d81351df8d678
richard261651 <richard261651@users.noreply.github.com>
2026-08-21 10:11:34 -0500

    chore: Actualizar sync_neon.bat

--- sync_neon.bat
+++ sync_neon.bat
@@ -1,8 +1,9 @@
 @echo off
-set "PATH=C:\Users\richard\NodeJS;C:\Users\richard\MinGit\cmd;%PATH%"
-cd /d "c:\Users\richard\Desktop\Repositorios\tienda"
+set "PATH=C:\Users\richa\Node;C:\Users\richa\MinGit\cmd;%PATH%"
+cd /d "%~dp0"
 
-echo Conectando a Neon PostgreSQL y creando tablas...
-call "C:\Users\richard\NodeJS\npx.cmd" prisma db push --accept-data-loss
+echo Conectando a Neon PostgreSQL y sincronizando tablas...
+call npx prisma db push --accept-data-loss
 echo.
-echo Proceso finalizado.
+echo Proceso finalizado con exito.
+pause
